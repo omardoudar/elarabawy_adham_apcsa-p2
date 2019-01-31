ce04c2b860992b4360c03bdc9238e955841f1ea3
Adham Elarabawy <adhamelarabawy@gmail.com>
2019-01-31 10:20:44 -0800

    init commit

---
+++ Unit0Students/Unit0Assignments/AsciiArt.java
@@ -0,0 +1,24 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class AsciiArt
+{
+	public static void main ( String[] args )
+	{
+		System.out.println("Adham Elarabawy \n\n" );
+		System.out.println("Rabbit" );
+		System.out.println("\n\n\n\n" );
+
+		System.out.println("           /");
+		System.out.println("         \\/");
+		System.out.println("         ()");
+		System.out.println("        -/\\-");
+		System.out.println("        _\\/_ ");
+
+	}
+}
