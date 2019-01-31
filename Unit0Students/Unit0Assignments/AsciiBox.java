91aadf5dfc51c08a36aa9da6661855165e2d4341
Adham Elarabawy <adhamelarabawy@gmail.com>
2019-01-31 10:24:18 -0800

    ascii box assignment

--- Unit0Students/Unit0Assignments/AsciiBox.java
+++ Unit0Students/Unit0Assignments/AsciiBox.java
@@ -11,7 +11,21 @@ public class AsciiBox
 	public static void main(String[] args)
 	{
 		System.out.println("name \t  date \n\n" );
-		System.out.println("+++++++++++++++++++++++++ " );
+		for(int i = 0; i < 4; i++){
+			System.out.println("++++++++++++++++++++++++++++++++++++ " );
+		}
+		for(int i = 0; i < 3; i++){
+			System.out.println("----------------------------------- " );
+		}
+		for(int i = 0; i < 2; i++){
+			System.out.println("***********************************  " );
+		}
+		for(int i = 0; i < 3; i++){
+			System.out.println("----------------------------------- " );
+		}
+		for(int i = 0; i < 4; i++){
+			System.out.println("+++++++++++++++++++++++++++++++++++ " );
+		}
 
 	}
 }
