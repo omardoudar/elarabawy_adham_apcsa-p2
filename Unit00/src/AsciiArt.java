5980248461d77e8667a3d6954e11b15778ff04f2
Adham Elarabawy <adhamelarabawy@gmail.com>
2019-02-12 11:06:43 -0800

    final formatting fix

---
+++ Unit00/src/AsciiArt.java
@@ -0,0 +1,26 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class AsciiArt {
+	public static void main(String[] args) {
+		// print info
+		System.out.println("Adham Elarabawy \n\n");
+		// print animal ascii name
+		System.out.println("Rabbit");
+		// create blank lines for spacing
+		System.out.println("\n\n\n\n");
+
+		// draw the actual rabbit
+		System.out.println("           /");
+		System.out.println("         \\/");
+		System.out.println("         ()");
+		System.out.println("        -/\\-");
+		System.out.println("        _\\/_ ");
+
+	}
+}
